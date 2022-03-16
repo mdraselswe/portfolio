a0784ed9a46155d9c260d098d7eb24afefffcf77
Muhammad Rasel <rrasel141@gmail.com>
2022-03-16 17:11:00 +0600

    design - services, portfolio and testimonials section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,86 @@
-import React from 'react'
-import './portfolio.css'
+import React from 'react';
+import './portfolio.css';
+import IMG1 from '../../assets/portfolio1.jpg';
+import IMG2 from '../../assets/portfolio2.jpg';
+import IMG3 from '../../assets/portfolio3.jpg';
+import IMG4 from '../../assets/portfolio4.jpg';
+import IMG5 from '../../assets/portfolio5.png';
+import IMG6 from '../../assets/portfolio6.jpg';
+
+// Do not use the img in production
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Natours Tour',
+    github: 'https://github.com/mdraselswe/Natours-Tour',
+    demo: 'https://mdraselswe.github.io/Natours-Tour/',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'React Hooks Todo App',
+    github: 'https://github.com/mdraselswe/React-Hoocks-TodoApp',
+    demo: 'https://wonderful-fermi-c52267.netlify.com/',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'The Movie DB',
+    github: 'https://github.com/mdraselswe/The-Movie-DB',
+    demo: 'https://cocky-hermann-09e9ce.netlify.app/',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Food Recipe',
+    github: 'hhttps://github.com/mdraselswe/Food_Recipe',
+    demo: 'https://reverent-lovelace-b1319a.netlify.com/',
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Crwn Clothing',
+    github: 'https://github.com/mdraselswe/crwn-clothing',
+    demo: 'https://crwn-clothing-rasel.herokuapp.com/',
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Money Management App',
+    github: 'https://github.com/mdraselswe/money-management-app',
+    demo: '#',
+  },
+];
 
 const Portfolio = () => {
-    return (
-        <section id="portfolio">
-            Portfolio
-        </section>
-    )
-}
+  return (
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className='container portfolio__container'>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className='portfolio__item'>
+              <div className='portfolio__item-image'>
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className='portfolio__item-cta'>
+                <a href={github} className='btn' target='_blank'>
+                  Github
+                </a>
+                <a href={demo} className='btn btn-primary' target='_blank'>
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
-import './testimonials.css'
-const Testimonials = () => {
-    return (
-        <section id="testimonials">
-            Testimonials
-        </section>
-    )
-}
+import React from 'react';
+import './testimonials.css';
+import AVTR1 from '../../assets/avatar1.jpg';
+import AVTR2 from '../../assets/avatar2.jpg';
+import AVTR3 from '../../assets/avatar3.jpg';
+import AVTR4 from '../../assets/avatar4.jpg';
 
-export default Testimonials
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Review 1',
+    review:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe voluptas vel aliquid in, nemo dolorum non autem? Explicabo, quibusdam repellendus.',
+  },
+  {
+    avatar: AVTR2,
+    name: 'Review 2',
+    review:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe voluptas vel aliquid in, nemo dolorum non autem? Explicabo, quibusdam repellendus.',
+  },
+  {
+    avatar: AVTR3,
+    name: 'Review 3',
+    review:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe voluptas vel aliquid in, nemo dolorum non autem? Explicabo, quibusdam repellendus.',
+  },
+  {
+    avatar: AVTR4,
+    name: 'Review 4',
+    review:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe voluptas vel aliquid in, nemo dolorum non autem? Explicabo, quibusdam repellendus.',
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        className='container testimonials__container'
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className='testimonial'>
+              <div className='client__avatar'>
+                <img src={avatar} />
+              </div>
+              <h5 className='client__name '>{name}</h5>
+              <small className='client__review'>{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
