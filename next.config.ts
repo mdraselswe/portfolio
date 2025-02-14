2c49b56c73c78311c10e9cc78024fbf53411226e
Muhammad Rasel <rrasel141@gmail.com>
2025-02-14 14:56:59 +0600

    update: deployment file

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "",
 };
 
 export default nextConfig;
